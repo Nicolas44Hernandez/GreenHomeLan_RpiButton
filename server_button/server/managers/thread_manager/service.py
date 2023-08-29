b390adbb280db69004ada7a57ac4c11b6fbaa39c
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-08-29 13:33:08 +0200

    send ka message periodicaly

--- server_button/server/managers/thread_manager/service.py
+++ server_button/server/managers/thread_manager/service.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import timedelta
 from timeloop import Timeloop
 from flask import Flask
 from server.interfaces.thread_interface import ThreadInterface
@@ -7,6 +8,7 @@ logger = logging.getLogger(__name__)
 
 thread_keep_alive_timeloop = Timeloop()
 
+DEVICE_ID=1
 
 class ThreadManager:
     """Manager for thread interface"""
@@ -17,6 +19,7 @@ class ThreadManager:
     thread_udp_port: int
     ipv6_mesh: str
     dataset_key: str
+    device_id: str
 
     def __init__(self, app: Flask = None) -> None:
         if app is not None:
@@ -32,6 +35,7 @@ class ThreadManager:
             self.thread_udp_port = app.config["THREAD_UDP_PORT"]
             self.ipv6_mesh = app.config["THREAD_IPV6_MESH"]
             self.dataset_key = app.config["THREAD_DATSET_KEY"]
+            self.device_id = DEVICE_ID
 
             # Create Thread interface
             self.thread_interface = ThreadInterface(
@@ -50,15 +54,6 @@ class ThreadManager:
             # send thread message to notify conncetion
             self.send_thread_message_to_border_router("ka_bt2")
 
-    # def schedule_thread_keep_alive_message_send(self):
-    #     """Schedule KA thread message"""
-
-    #     @thread_keep_alive_timeloop.job(interval=timedelta(seconds=20))
-    #     def send_keep_alive():
-    #         logger.info("sending Thread keep alive msg")
-    #         self.send_thread_message_to_border_router("ka_cam")
-
-    #     thread_keep_alive_timeloop.start(block=False)
 
     def send_thread_message_to_border_router(self, message: str):
         """Send message to border router"""
@@ -70,6 +65,21 @@ class ThreadManager:
                 "Thread network not configured or not running, wating for network setup message"
             )
             logger.error("Message not published")
+
+    def send_keep_alive_message(self):
+        """Send keep alive message via thread"""
+        message = f"ka_{self.device_id}"
+        logger.info("sending Thread keep alive msg")
+        self.send_thread_message_to_border_router(message)
+
+    def schedule_thread_keep_alive_message_send(self):
+        """Schedule KA thread message"""
+
+        @thread_keep_alive_timeloop.job(interval=timedelta(seconds=20))
+        def send_keep_alive():
+            self.send_keep_alive_message()
+
+        thread_keep_alive_timeloop.start(block=False)
 
 
 thread_manager_service: ThreadManager = ThreadManager()
